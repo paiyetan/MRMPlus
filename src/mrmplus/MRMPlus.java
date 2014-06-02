03396bc50a496ab10b14fa765e59aa0ac1ed7cb7
U-paiyeta1-PC\paiyeta1 <paiyeta1@PATH-CR-BQ25CS1.win.ad.jhu.edu>
2014-06-02 11:52:52 -0400

    initial commit...

---
+++ src/mrmplus/MRMPlus.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mrmplus;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author paiyeta1
+ */
+public class MRMPlus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        // TODO code application logic here
+        LinkedList<PeptideRecord> peptideRecord;
+        HashMap<String,PeptideRecord> idPep2RecordsMap;
+        
+    }
+}
---
+++ src/mrmplus/PeptideRecord.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mrmplus;
+
+/**
+ *
+ * @author paiyeta1
+ */
+public class PeptideRecord {
+    
+    //private 
+    
+}
